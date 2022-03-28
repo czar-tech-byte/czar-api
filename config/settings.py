237992ab97bc381e422ba8bb3fde50162ef39ef7
czar-tech-byte <kenyaczar@gmail.com>
2022-03-28 22:18:37 +0300

    corrections

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'czar',
+    'rest_framework',
+    'czar'
 ]
 
 MIDDLEWARE = [
--- czar/models.py
+++ czar/models.py
@@ -32,3 +32,28 @@ class Product(models.Model):
     precise_location = models.CharField(max_length=200)
     phone_number = PhoneNumberField(null = True, blank = True)
     negotiatable = models.BooleanField(default=False)
+
+def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order_details")
+    username = models.CharField(max_length=100)
+    order_id = models.IntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name="order_details")
+    address_type = models.CharField(max_length=100,choices=[('BILLING', 'Billing'), ('SHIPPING', 'Shipping')])
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100)
+    total = models.IntegerField(default=0)
+    numincart = models.IntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name="cart")
+
+    def __str__(self):
+        return self.username
